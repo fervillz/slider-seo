e596b81ce53ae3f4e0c53f12e108256d4553638d
fernando villamor jr <fervillz@gmail.com>
2016-04-15 04:59:37 -0700

    lantapan update

--- admin/views/gsettings_partials/slider_type.php
+++ admin/views/gsettings_partials/slider_type.php
@@ -13,14 +13,23 @@
 				<label class="label_slider_type label_slider_type2" for="slider_type2"></label>
 				<input type="radio" class="radio_slider_type" name="slider_type" id="slider_type3" value = "3" <?php echo (get_post_meta( $post->ID, 'slider_type', true ) === '3') ? 'checked' :  '' ; ?>>
 				<label class="label_slider_type label_slider_type3" for="slider_type3"></label>
+
+				<input type="radio" class="radio_slider_type" name="slider_type" id="slider_type4" value = "4" <?php echo (get_post_meta( $post->ID, 'slider_type', true ) === '4') ? 'checked' :  '' ; ?>>
+				<label class="label_slider_type label_slider_type4" for="slider_type4"></label>
+
+				<input type="radio" class="radio_slider_type" name="slider_type" id="slider_type5" value = "5" <?php echo (get_post_meta( $post->ID, 'slider_type', true ) === '5') ? 'checked' :  '' ; ?>>
+				<label class="label_slider_type label_slider_type5" for="slider_type5"></label>
+
+				<input type="radio" class="radio_slider_type" name="slider_type" id="slider_type6" value = "6" <?php echo (get_post_meta( $post->ID, 'slider_type', true ) === '6') ? 'checked' :  '' ; ?>>
+				<label class="label_slider_type label_slider_type6" for="slider_type6"></label>
+			
 			<?php else: ?>
 				<input type="radio" class="radio_slider_type" name="slider_type" id="slider_type1" value ="1" checked>
 			<?php endif; ?>
 		</td>
 	<?php elseif ( $tab == 'advanced' ): ?>
 		<td class="slider-type-radio">
-			<input type="radio" class="radio_slider_type" name="slider_type" id="slider_type4" value = "1" <?php echo (get_post_meta( $post->ID, 'slider_type', true ) === '4') ? 'checked' :  '' ; ?>>
-			<label class="label_slider_type label_slider_type4" for="slider_type4"></label>
+			
 
 		</td>
 	<?php endif; ?>
